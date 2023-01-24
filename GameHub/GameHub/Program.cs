ef4b70599f20a28dfc6e4db2ec68f1198aafd81a
Luana Oliveira <luanasilva.jua@gmail.com>
2023-01-24 20:53:04 -0300

    deleting comments

--- GameHub/GameHub/Program.cs
+++ GameHub/GameHub/Program.cs
@@ -61,45 +61,6 @@ namespace GameHub
                 }
                 count ++;
             } while (true);
-
-            /*Console.WriteLine("Jogador1 == X | Jogador2 == O");
-            Console.WriteLine("\n");
-            Console.WriteLine($"{posicao[0]} {posicao[1]} {posicao[2]}");
-            Console.WriteLine($"{posicao[3]} {posicao[4]} {posicao[5]}");
-            Console.WriteLine($"{posicao[6]} {posicao[7]} {posicao[8]}");
-            Console.WriteLine("\n");
-
-            for (int i = 0; i <= posicao.Length; i++)
-            {
-                Console.WriteLine("Digite a posição do Jogador1");
-                jogadorX = Console.ReadLine();
-
-                if (jogadorX == posicao[i])
-                    {
-                        posicao[i] = "X";
-
-                        Console.WriteLine($"{posicao[0]} {posicao[1]} {posicao[2]}");
-                        Console.WriteLine($"{posicao[3]} {posicao[4]} {posicao[5]}");
-                        Console.WriteLine($"{posicao[6]} {posicao[7]} {posicao[8]}");
-                    }
-            } */
-
-           /*int[,] posicao = new int[3, 3];
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.WriteLine("Escolha");
-
-                    posicao[i, j] = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"{posicao[i, j]}");
-                }
-            }
-
-            Console.WriteLine($"{posicao[0, 0]} {posicao[0, 1]} {posicao[0, 2]}");
-            Console.WriteLine($"{posicao[1, 0]} {posicao[1, 1]} {posicao[1, 2]}");
-            Console.WriteLine($"{posicao[2, 0]} {posicao[2, 1]} {posicao[2, 2]}");*/
         }
     }
 }
